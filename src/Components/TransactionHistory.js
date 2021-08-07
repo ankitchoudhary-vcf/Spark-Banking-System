4b302b42285b4ffdd13f66a288f9de8e0285d221
ankit <ac2000choudhary@gmail.com>
2021-08-07 20:14:43 +0530

    TRansaction History updated

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import { Statements } from './Statements'
- 
-export const TransactionHistory = () => {
-    return (
-        <>
+import React from "react";
+import { Statements } from "./Statements";
+import { useState, useEffect } from 'react';
+
+export const TransactionHistory = (props) => {
+    
+    const [statements, setStatements] = useState([]);
+
+    useEffect(() =>{
+        function filteredStatements(data){
+            return data.Status === "Created"
+        }
+
+        async function Statements() {
+            var filtered = await props.transactionDetails.filter(filteredStatements);
+            setStatements(filtered)
+        }
+        Statements()
+    }, [props.transactionDetails]);
+
+  return (
+    <>
       <div className="mt-6 notification mx-2 is-success is-light title has-text-centered">
         Transactions History
         <hr className="has-background-success" />
@@ -12,33 +28,31 @@ export const TransactionHistory = () => {
         <div className="column is-12">
           <div className="columns notification has-text-centered is-mobile is-dark p-0">
             <div className="column is-3">
-              <h3 className="title is-size-5-touch is-size-3-desktop">
-                Date
-              </h3>
+              <h3 className="title is-size-5-touch is-size-3-desktop">Date</h3>
             </div>
-            <div className="column is-3">
+            <div className="column is-6">
               <h3 className="title is-size-5-touch is-size-3-desktop">
                 Description
               </h3>
             </div>
             <div className="column is-3">
-              <h3 className="title is-size-5-touch is-size-3-desktop">
-                Dr.
-              </h3>
-            </div>
-            <div className="column is-3">
-              <h3 className="title is-size-5-touch is-size-3-desktop">
-                Cr.
-              </h3>
+              <h3 className="title is-size-5-touch is-size-3-desktop">Amount</h3>
             </div>
           </div>
-          <hr className="has-background-dark"/>
-          <hr className="has-background-dark"/>
+          <hr className="has-background-dark" />
+          <hr className="has-background-dark" />
         </div>
 
-        <Statements />
-
+        {statements.length === 0 ? (
+          <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
+            No Transaction Yet
+          </div>
+        ) : (
+          statements.map((transactions) => {
+            return <Statements key={transactions._id} statements={transactions} />
+          })
+        )}
       </div>
     </>
-    )
-}
+  );
+};
