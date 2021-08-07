c0931076382ffeb5d6dfcc06c8468a8ced6f00f1
ankit <ac2000choudhary@gmail.com>
2021-08-07 17:52:27 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ import "bulma/css/bulma.min.css";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { Footer } from "./Components/Footer.js";
 import { CustomersDetails } from "./Components/CustomersDetails";
-import { TransactionHistory } from "./Components/TransactionHistory"
+import { TransactionHistory } from "./Components/TransactionHistory";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  //customersDetails
+  const [customersDetails, setCustomersDetails] = useState([]);
+
+  //loading the customersDetails
+  useEffect(() => {
+    async function fetchCustomersDetails() {
+      try {
+        const data = await axios.get("http://localhost:8080/customers");
+        setCustomersDetails(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchCustomersDetails();
+  }, [customersDetails]);
+
   // Burger event handler
   const handleBurger = (e) => {
     var navbar = document.querySelector("#" + e.target.dataset.target);
@@ -24,7 +42,7 @@ function App() {
             <Home />
           </Route>
           <Route exact path="/custom-details">
-            <CustomersDetails />
+            <CustomersDetails customersDetails={customersDetails} />
           </Route>
           <Route exact path="/transaction-statements">
             <TransactionHistory />
