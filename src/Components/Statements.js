557771911325fae5b7f46b61710b6de874a8bae3
ankit <ac2000choudhary@gmail.com>
2021-08-07 13:44:58 +0530

    Statements Component added

---
+++ src/Components/Statements.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Statements = () => {
+  return (
+    <>
+      <div className="column is-12">
+        <div className="columns has-text-centered is-mobile p-0">
+          <div className="column is-3">
+            <h3 className="title is-size-5-touch is-size-3-desktop">
+              12-10-2021
+            </h3>
+          </div>
+          <div className="column is-3">
+            <h3 className="title is-size-5-touch is-size-3-desktop">
+              Transferred by account no. 101
+            </h3>
+          </div>
+          <div className="column is-3">
+            <h3 className="title is-size-5-touch is-size-3-desktop">
+                -
+            </h3>
+          </div>
+          <div className="column is-3">
+            <h3 className="title is-size-5-touch is-size-3-desktop">
+                1,600
+            </h3>
+          </div>
+        </div>
+        <hr className="has-background-dark" />
+      </div>
+    </>
+  );
+};
