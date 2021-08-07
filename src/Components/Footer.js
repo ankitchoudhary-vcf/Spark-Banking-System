5daae08611c430831ac33bd4dc99be9990e9bd28
ankit <ac2000choudhary@gmail.com>
2021-08-07 13:44:06 +0530

    Footer added

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Footer = () => {
+  const footerStyle = {
+    position: "relative",
+    top: "16vh",
+    width: "100%",
+  };
+
+  return (
+    <footer className="footer has-background-dark" style={footerStyle}>
+      <div class="content has-text-centered has-text-white">
+        <p>
+          <strong className="has-text-white">Banking System</strong> created by{" "}
+          <a href="https://github.com/ankit1509">Ankit Choudhary</a>.
+        </p>
+      </div>
+    </footer>
+  );
+};
