5c9f6eee90da43a348d0b6770362077de75cfe2f
ankit <ac2000choudhary@gmail.com>
2021-08-07 17:52:20 +0530

    Details updated

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,25 +1,25 @@
 import React from "react";
 
-export const Details = () => {
+export const Details = (props) => {
   return (
     <>
       <div className="column is-12">
         <div className="columns has-text-centered is-mobile p-0">
           <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">101</h3>
+            <h3 className="title is-size-5-touch is-size-3-desktop">{props.customer.Account_No}</h3>
           </div>
           <div className="column is-3">
             <h3 className="title is-size-5-touch is-size-3-desktop">
-              Rahul Kumar
+            {props.customer.Account_Holder}
             </h3>
           </div>
           <div className="column is-3">
             <h3 className="title is-size-5-touch is-size-3-desktop">
-              rahul@gmail.com
+            {props.customer.Email_Id}
             </h3>
           </div>
           <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">16,500</h3>
+            <h3 className="title is-size-5-touch is-size-3-desktop">₹{props.customer.Balance}</h3>
           </div>
         </div>
         <hr className="has-background-dark" />
