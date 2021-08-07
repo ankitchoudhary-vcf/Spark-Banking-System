da6ff5d1a188cf1add1f434f2c9f571e8739ce93
ankit <ac2000choudhary@gmail.com>
2021-08-07 13:45:06 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import "bulma/css/bulma.min.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { Footer } from "./Components/Footer.js";
+import { CustomersDetails } from "./Components/CustomersDetails";
+import { TransactionHistory } from "./Components/TransactionHistory"
 
 function App() {
+  // Burger event handler
+  const handleBurger = (e) => {
+    var navbar = document.querySelector("#" + e.target.dataset.target);
+    navbar.classList.toggle("is-active");
+    e.target.classList.toggle("is-active");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Header handleBurger={handleBurger} />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/custom-details">
+            <CustomersDetails />
+          </Route>
+          <Route exact path="/transaction-statements">
+            <TransactionHistory />
+          </Route>
+        </Switch>
+        <Footer />
+      </HashRouter>
+    </>
   );
 }
 
