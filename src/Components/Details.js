59a5f4c098b4c2d14b587c0de89a87efdceab569
ankit <ac2000choudhary@gmail.com>
2021-08-08 12:59:29 +0530

    Details updated

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,28 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 export const Details = (props) => {
+
   return (
     <>
-      <div className="column is-12">
-        <div className="columns has-text-centered is-mobile p-0">
-          <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">{props.customer.Account_No}</h3>
+      <Link to='/customer-details' className="column is-12 table my-0 is-hover" data-acc={props.customer.Account_No} onClick={(e) => {props.setCurrentCustomer(e.target.dataset.acc)}}>
+        <div className="columns has-text-centered is-mobile p-0" data-acc={props.customer.Account_No}>
+          <div className="column is-3 px-0" data-acc={props.customer.Account_No}>
+            <h3 className="title is-size-7-touch is-size-3-desktop" data-acc={props.customer.Account_No}>{props.customer.Account_No}</h3>
           </div>
-          <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">
+          <div className="column is-3 px-0" data-acc={props.customer.Account_No}>
+            <h3 className="title is-size-7-touch is-size-3-desktop" data-acc={props.customer.Account_No}>
             {props.customer.Account_Holder}
             </h3>
           </div>
-          <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">
+          <div className="column is-3 px-0" data-acc={props.customer.Account_No}>
+            <h3 className="title is-size-7-touch is-size-3-desktop" data-acc={props.customer.Account_No}>
             {props.customer.Email_Id}
             </h3>
           </div>
-          <div className="column is-3">
-            <h3 className="title is-size-5-touch is-size-3-desktop">₹{props.customer.Balance}</h3>
+          <div className="column is-3 px-0" data-acc={props.customer.Account_No}>
+            <h3 className="title is-size-7-touch is-size-3-desktop" data-acc={props.customer.Account_No}>₹{props.customer.Balance}</h3>
           </div>
         </div>
-        <hr className="has-background-dark" />
+      </Link>
+      <div className="column is-12 my-0 py-0">
+      <hr className="has-background-dark" />
+
       </div>
     </>
   );
