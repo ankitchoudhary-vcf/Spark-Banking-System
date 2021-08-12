a4423259ade234095c4d66cd53405c0e122a4eef
ankit <ac2000choudhary@gmail.com>
2021-08-12 11:40:38 +0530

    updated

--- src/Components/CustomersDetails.js
+++ src/Components/CustomersDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
+import { BeatLoader } from "react-spinners";
 import { Details } from "./Details";
-
 export const CustomersDetails = (props) => {
   return (
     <>
@@ -8,7 +8,7 @@ export const CustomersDetails = (props) => {
         Customers Details
         <hr className="has-background-success" />
       </div>
-      <div className="columns notification is-light mx-2 is-mobile is-multiline">
+      <div className="columns notification is-light mx-2 is-mobile is-multiline" style={{justifyContent: 'center'}}>
         <div className="column is-12">
           <div className="columns notification has-text-centered is-mobile is-dark p-0">
             <div className="column is-3 px-0">
@@ -36,16 +36,25 @@ export const CustomersDetails = (props) => {
           <hr className="has-background-dark" />
         </div>
 
-        {props.customersDetails.length === 0 ? (
-          <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
-            {" "}
-            No Data to Display{" "}
-          </div>
-        ) : (
-          props.customersDetails.map((customer) => {
-            return <Details key={customer._id} customer={customer} setCurrentCustomer={props.setCurrentCustomer} currentCustomer={props.currentCustomer} />;
-          })
+        {!props.loading ? (
+          <>
+            <BeatLoader size={72} color="orange" loading />
+          </>
+        ):(
+          props.customersDetails.length === 0 ? (
+            <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
+              {" "}
+              No Data to Display{" "}
+            </div>
+          ) : (
+            props.customersDetails.map((customer) => {
+              return <Details key={customer._id} customer={customer} setCurrentCustomer={props.setCurrentCustomer} currentCustomer={props.currentCustomer} />;
+            })
+          )
         )}
+
+        
+
       </div>
     </>
   );
