04db1001b82ca2f58af381eea74c51c7151c8ddf
ankit <ac2000choudhary@gmail.com>
2021-08-12 11:40:51 +0530

    updated

--- src/Components/TransactionHistory.js
+++ src/Components/TransactionHistory.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Statements } from "./Statements";
+import { BeatLoader } from "react-spinners";
 import { useState, useEffect } from 'react';
 
 export const TransactionHistory = (props) => {
@@ -25,7 +26,7 @@ export const TransactionHistory = (props) => {
         Transactions History
         <hr className="has-background-success" />
       </div>
-      <div className="columns notification is-light mx-2 is-mobile is-multiline">
+      <div className="columns notification is-light mx-2 is-mobile is-multiline" style={{justifyContent: 'center'}}>
         <div className="column is-12">
           <div className="columns notification has-text-centered is-mobile is-dark p-0">
             <div className="column is-3 px-0">
@@ -44,15 +45,23 @@ export const TransactionHistory = (props) => {
           <hr className="has-background-dark" />
         </div>
 
-        {statements.length === 0 ? (
-          <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
-            No Transaction Yet
-          </div>
-        ) : (
-          statements.map((transactions) => {
-            return <Statements key={transactions._id} statements={transactions} />
-          })
+        {!props.loading ? (
+          <>
+            <BeatLoader loading color="orange" size={72} />
+          </>
+        ):(
+          statements.length === 0 ? (
+            <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
+              No Transaction Yet
+            </div>
+          ) : (
+            statements.map((transactions) => {
+              return <Statements key={transactions._id} statements={transactions} />
+            })
+          )
         )}
+
+        
       </div>
     </>
   );
