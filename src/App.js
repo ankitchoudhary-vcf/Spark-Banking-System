2683311e2c89482c8280e516febeff1c0a6b28b3
ankit <ac2000choudhary@gmail.com>
2021-08-13 11:27:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     async function fetchCustomersDetails() {
       try {
         const data = await axios.get(
-          "https://banking-server.herokuapp.com/customers"
+          process.env.React_App_API_KEY+"/customers"
         );
         setCustomersDetails(data.data);
         setCustomerLoading(true);
@@ -48,7 +48,7 @@ function App() {
     async function fetchTransactionDetails() {
       try {
         const transactionData = await axios.get(
-          "https://banking-server.herokuapp.com/transactions"
+          process.env.React_App_API_KEY +"/transactions"
         );
         setTransactionDetails(transactionData.data);
         setTransactionLoading(true);
@@ -80,14 +80,14 @@ function App() {
 
       try {
         const transfer = await axios.put(
-          `https://banking-server.herokuapp.com/customers/${data.from}&${data.to}`,
+          process.env.React_App_API_KEY+`/customers/${data.from}&${data.to}`,
           {
             amount: data.amount,
           }
         );
         if (transfer.status === 200 && !transfer.data.message) {
           const response = await axios.post(
-            "https://banking-server.herokuapp.com/transactions",
+            process.env.React_App_API_KEY+"/transactions",
             data
           );
           if (response.status === 200) {
