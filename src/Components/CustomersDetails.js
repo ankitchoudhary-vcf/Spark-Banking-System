257c42c56a4756ee8ce18c78ddefc3559284fe68
ankit <ac2000choudhary@gmail.com>
2021-08-13 11:22:04 +0530

    updated

--- src/Components/CustomersDetails.js
+++ src/Components/CustomersDetails.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BeatLoader } from "react-spinners";
 import { Details } from "./Details";
 export const CustomersDetails = (props) => {
+  
+  const [customers, setCustomer] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setCustomer(props.customersDetails);
+    setLoading(true);
+  }, [props.customersDetails]);
+
   return (
     <>
       <div className="mt-6 notification mx-2 is-success is-light title has-text-centered">
         Customers Details
         <hr className="has-background-success" />
       </div>
-      <div className="columns notification is-light mx-2 is-mobile is-multiline" style={{justifyContent: 'center'}}>
+      <div
+        className="columns notification is-light mx-2 is-mobile is-multiline"
+        style={{ justifyContent: "center" }}
+      >
         <div className="column is-12">
           <div className="columns notification has-text-centered is-mobile is-dark p-0">
             <div className="column is-3 px-0">
@@ -36,25 +48,27 @@ export const CustomersDetails = (props) => {
           <hr className="has-background-dark" />
         </div>
 
-        {!props.loading ? (
+        {!loading ? (
           <>
             <BeatLoader size={72} color="orange" loading />
           </>
-        ):(
-          props.customersDetails.length === 0 ? (
-            <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
-              {" "}
-              No Data to Display{" "}
-            </div>
-          ) : (
-            props.customersDetails.map((customer) => {
-              return <Details key={customer._id} customer={customer} setCurrentCustomer={props.setCurrentCustomer} currentCustomer={props.currentCustomer} />;
-            })
-          )
+        ) : customers.length === 0 ? (
+          <div className="has-background-danger column is-12 has-text-centered title is-size-6-touch is-size-4-desktop">
+            {" "}
+            No Data to Display{" "}
+          </div>
+        ) : (
+          customers.map((customer) => {
+            return (
+              <Details
+                key={customer._id}
+                customer={customer}
+                setCurrentCustomer={props.setCurrentCustomer}
+                currentCustomer={props.currentCustomer}
+              />
+            );
+          })
         )}
-
-        
-
       </div>
     </>
   );
